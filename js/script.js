94c76d8ade0cadbb370ebc5605a3bff8d0c8e0d9
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-01-17 18:19:37 +0300

    it will add delivery section

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $(".p1").click(function () {
+        $(".sicilian").show();
+        $(".p1").hide();
+    
+    });
+    $(".sicilian").click(function () {
+        $(".p1").show();
+        $(".sicilian").hide();
+    })
+    });
+
+    $(".col-md-3").mouseover(function () {
+        $(".p1").show();
+    });
+    $(".p1").mouseout(function () {
+        $(".col-md-3").hide();
+    });
+    
+    function order(){
+        var size = document.getElementById("size").value;
+        var crust = document.getElementById("crust").value;
+        var quantity = document.getElementById("quantity").value;
+        var topping = document.getElementById("topping").value;
+        var call= getSize(); getCrust(); getToppping(); getQuantity(); getDelivery(); 
+        var delivery = document.getElementById("delivery").value;
+        var  calculate = (parseInt(size) + parseInt(crust) + parseInt(topping)) * quantity;
+        
+        
+        
+        function getSize(){
+            if(size =="") {
+            alert("please place pizza-size");
+            return false;
+        }
+    
